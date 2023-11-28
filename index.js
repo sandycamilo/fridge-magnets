37474b35c8491e97705813652c0214d9f4576173
sandycamilo <39415553+sandycamilo@users.noreply.github.com>
2023-11-28 13:34:42 -0500

    u

--- index.js
+++ index.js
@@ -26,40 +26,42 @@ function makeDraggable(element) {
     isDragging = false;
   });
 
-  document.addEventListener("DOMContentLoaded", function () {
-    const draggables = document.querySelectorAll(".draggable");
+  element.addEventListener("touchstart", function (e) {
+    e.preventDefault();
+    isDragging = true;
 
-    draggables.forEach((draggable) => {
-      let initialX;
-      let initialY;
+    const touch = e.touches[0];
+    offsetX = touch.clientX - parseFloat(getComputedStyle(element).left);
+    offsetY = touch.clientY - parseFloat(getComputedStyle(element).top);
+  });
 
-      let offsetX = 0;
-      let offsetY = 0;
+  document.addEventListener("touchmove", function (e) {
+    e.preventDefault();
 
-      draggable.addEventListener("touchstart", (e) => {
-        e.preventDefault();
+    if (isDragging) {
+      const touch = e.touches[0];
+      var x = touch.clientX - offsetX;
+      var y = touch.clientY - offsetY;
 
-        initialX = e.touches[0].clientX;
-        initialY = e.touches[0].clientY;
+      x = Math.min(window.innerWidth - element.width, Math.max(0, x));
+      y = Math.min(window.innerHeight - element.height, Math.max(0, y));
 
-        const rect = draggable.getBoundingClientRect();
-        offsetX = initialX - rect.left;
-        offsetY = initialY - rect.top;
-      });
+      element.style.left = x + "px";
+      element.style.top = y + "px";
+    }
+  });
 
-      draggable.addEventListener("touchmove", (e) => {
-        e.preventDefault();
-
-        const currentX = e.touches[0].clientX - offsetX;
-        const currentY = e.touches[0].clientY - offsetY;
-
-        draggable.style.left = `${currentX}px`;
-        draggable.style.top = `${currentY}px`;
-      });
-    });
+  document.addEventListener("touchend", function () {
+    isDragging = false;
   });
 }
-  var draggableElements = document.querySelectorAll(".draggable");
-  draggableElements.forEach(function (element) {
-    makeDraggable(element);
-  });
+
+var draggableElements = document.querySelectorAll(".draggable");
+draggableElements.forEach(function (element) {
+  makeDraggable(element);
+});
+
+var draggableElements = document.querySelectorAll(".draggable");
+draggableElements.forEach(function (element) {
+makeDraggable(element);
+});
